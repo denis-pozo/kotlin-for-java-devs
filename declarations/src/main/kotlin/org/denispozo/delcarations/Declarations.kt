4751aa3f75d094b142c81399ea3fe429a58431a2
Denis Pozo <denis@mbr-targeting.com>
2023-10-05 13:52:49 +0200

    Add differences between java and kotlin in the declarations' main

--- declarations/src/main/kotlin/org/denispozo/delcarations/Declarations.kt
+++ declarations/src/main/kotlin/org/denispozo/delcarations/Declarations.kt
@@ -53,6 +53,80 @@ fun main(args: Array<String>) {
 
     // 10. new keyword doesn't exist!!
 
+    // 11. Kotlin handles equals differently. == and equals are the same and check for structural equality
+    val employeeOne = Employee("Mary", 1)
+    val employeeTwo = Employee("John", 2)
+    val employeeThree = Employee("John", 2)
+
+    println("STRUCTURALLY EQUALS")
+    println(employeeOne == employeeTwo)
+    println(employeeTwo == employeeThree) // This one would be false in java
+    println(employeeOne.equals(employeeTwo))
+    println(employeeTwo.equals(employeeThree))
+
+    // For referential equality there is the triple equal operator: ===
+    println("REFERENTIALLY EQUAL")
+    println(employeeOne === employeeTwo)
+    println(employeeTwo === employeeThree) // This one would be false in java
+    val employeeFour = employeeTwo
+    println(employeeTwo === employeeFour) // This one would be false in java
+
+    // Same for not equals
+    println("NOT EQUAL")
+    println(employeeOne != employeeTwo)
+    println(employeeTwo !== employeeFour)
+    println(employeeTwo !== employeeThree)
+
+    // 12. Bit operators | -> or, & -> and
+    val x = 0x00110101
+    val y = 0x10011000
+
+    println ("X or Y: " + (x or y))
+    println ("X and Y: " + (x and y))
+
+    // 13. Casting: instance of -> is and no need for casting
+    val something: Any = employeeFour
+    if (something is Employee) {
+        // val newEmployee = something as Employee - It's not needed
+        println("Employee four is actually an employee called ${employeeFour.name}")
+    }
+
+    // 14. String templates
+    println(employeeOne)
+
+    val change = 4.22
+    println("Your change in \$ is $$change")
+
+    val numerator = 10.99
+    val denominator = 20.00
+    println("The value of $numerator divided by $denominator is ${numerator/denominator}")
+
+    // 15. Raw strings or triple quoted strings
+    val filePath = """c:\somedir\somedir2"""
+    println(filePath)
+
+    val nurseryRhyme = """Humpty Dympty sat on the wall
+                                *Humpty Dumpty had a great fall
+                                *All the king's horses and all the king's men
+                                *couldn't put Humpy together again.""".trimMargin("*")
+    println(nurseryRhyme)
+
+    // 16. Read, Eval, and Print Loop -> REPL
+    // Tools -> Kotlin -> Kotlin REPL
+
 }
 
-data class Employee(var name: String, val id: Int)
+class Employee(var name: String, val id: Int) {
+
+    override fun toString(): String {
+        return "Employee(name=$name, id=$id)" // String templates
+    }
+
+    override fun equals(obj: Any?): Boolean {
+        if (obj is Employee) {
+            return name == obj.name && id == obj.id
+        }
+
+        return false
+    }
+}

64d6f6af23d796c2c32089f9f5677caf534f121e
Denis Pozo <denis@mbr-targeting.com>
2023-10-05 10:01:41 +0200

    First commit - add text adventure kotlin app

---
+++ text-adventure-kotlin/src/main/kotlin/org/denispozo/kotlintextadventure/Location.kt
@@ -0,0 +1,15 @@
+package org.denispozo.kotlintextadventure
+
+data class Location (val locationID: Int, val description: String) {
+
+    val exits = mutableMapOf<String, Int>()
+
+    init {
+        exits["Q"] = 0
+    }
+
+    fun addExit(direction: String, destinationID: Int) {
+        exits[direction] = destinationID
+    }
+}
+
---
+++ text-adventure-kotlin/src/main/kotlin/org/denispozo/kotlintextadventure/Locations.kt
@@ -0,0 +1,21 @@
+package org.denispozo.kotlintextadventure
+
+import java.io.File
+
+fun readLocationInfo() : Map<Int, Location> {
+    val locations = mutableMapOf<Int, Location>()
+
+    File("src/main/resources/locations_big.txt").reader().forEachLine {
+        val tokens = it.split("`")
+        val location = Location(tokens[0].toInt(), tokens[1])
+        locations[location.locationID] = location
+    }
+
+    File("src/main/resources/directions_big.txt").reader().forEachLine {
+        val tokens = it.split(",")
+
+        locations[tokens[0].toInt()]?.addExit(tokens[1], tokens[2].toInt())
+    }
+
+    return locations
+}
